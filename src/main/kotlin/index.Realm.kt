d4c47529129312e4d2111607fb6b6a9db3ba7044
Ben Chatelain <ben@octop.ad>
2017-08-22 17:19:10 -0600

    Remove comma to fix error

--- src/main/kotlin/index.Realm.kt
+++ src/main/kotlin/index.Realm.kt
@@ -1,5 +1,5 @@
 @file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS")
-@file:[JsQualifier("Realm"), JsModule("realm")]
+@file:[JsQualifier("Realm") JsModule("realm")]
 package Realm
 
 import kotlin.js.*
