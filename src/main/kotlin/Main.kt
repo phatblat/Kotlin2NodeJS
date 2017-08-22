8f990324d39d434db1bcf7d05cd52493eb95d908
Ben Chatelain <ben@octop.ad>
2017-08-22 16:19:19 -0600

    Add Realm connection to ROS using promise

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,68 @@
-external fun require(module:String):dynamic
+import kotlin.js.Promise
+//import realm.Realm
+
+// TODO: Change alias after realm js library stubs successfully imported.
+typealias RealmType = Any
+
+external fun require(module: String): dynamic
+
+val Realm = require("realm")
+
+val username    = "test@imac.local"
+val password    = "password"
+val hostname    = "imac.local"
+val server_url  = "http://$hostname:9080"
+val realm_url   = "realm://$hostname:9080/~/RealmNode"
 
 fun main(args: Array<String>) {
-    println("Hello JavaScript!")
+    println("main(), args: $args")
 
-    val express = require("express")
-    val app = express()
+    val realmPromise = Promise<RealmType>({ resolve, reject ->
+        if (!Realm.Sync.User.current) {
+            console.log("Logging into realm object server $server_url as user $username")
 
-    app.get("/", { req, res ->
-        res.type("text/plain")
-        res.send("I am a beautiful butterfly")
+            Realm.Sync.User.login(server_url, username, password, { error, user ->
+                if (error) {
+                    console.log("Error logging in")
+                    reject(Exception("Error logging in"))
+                }
+
+                console.log("Successfully logged in")
+                resolve(openRealm(Realm.Sync.User.current))
+            })
+        }
+
+        console.log("Realm user ${Realm.Sync.User.current.identity} is currently logged in")
+        resolve(openRealm(Realm.Sync.User.current))
     })
+}
 
-    app.listen(3000, {
-        println("Listening on port 3000")
-    })
+fun openRealm(user: User) {
+    val config = mapOf(
+        "sync" to mapOf(
+            "user" to user,
+            "url" to realm_url
+        ),
+        "schema" to arrayOf(
+            UserSchema()
+        )
+    )
+
+    // Open the realm
+    // synchronously
+    // module.exports.realm = new Realm(config)
+
+    // asynchronously
+    // Realm.openAsync(config, (error, realm) => {
+    //     if (error) {
+    //          console.log("Error connecting to realm at url: " + realm_url)
+    //          return
+    //     }
+    //
+    //     console.log("Connected to realm at url: " + realm_url)
+    //     module.exports.realm = realm
+    // })
+
+    // as a Promise
+    return Realm.open(config)
 }
