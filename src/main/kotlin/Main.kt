65c36c8a4e94c1e036fda7be43fcae6419beca78
Ben Chatelain <ben@octop.ad>
2017-08-22 20:34:28 -0600

    Extract login() function, use promises to chain everything in Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,30 +14,62 @@ val realm_url   = "realm://$hostname:9080/~/RealmNode"
 fun main(args: Array<String>) {
     println("main(), args: $args")
 
-    val realmPromise = Promise<Realm>({ resolve, reject ->
-        if (User.current == null) {
-            console.log("Logging into realm object server $server_url as user $username")
-
-            User.login(server_url, username, password, { error, user ->
-                if (error != null) {
-                    console.log("Error logging in")
-                    reject(Exception("Error logging in"))
-                }
-
-                console.log("Successfully logged in")
-                openRealm(User.current).then({ realm ->
-                    resolve(realm)
-                })
+    Promise<Realm>({ resolve, reject ->
+        login(server_url, username, password)
+        .then({ user ->
+            openRealm(user).then({ realm ->
+                resolve(realm)
             })
-        }
+        })
+        .catch({ error ->
+            console.log(error)
+            reject(error)
+        })
+    })
+    .then({
+        User.current?.logout()
+        console.log("Sync user logged out")
+//        process.exit()
+    })
+    .catch({ error ->
+        console.log("Error: " + error)
+//        process.exit(1)
+    })
+}
 
-        console.log("Realm user ${User.current.identity} is currently logged in")
-        openRealm(User.current).then({ realm ->
-            resolve(realm)
+/**
+ * Private function which logs a user into an object server.
+ * @return Promise that resolves to a realm.
+ */
+fun login(server_url: String, username: String, password: String): Promise<User> {
+    var current_user = User.current
+    if (current_user != null) {
+        console.log("Realm user ${current_user.identity} is currently logged in")
+        return Promise.resolve(current_user)
+    }
+
+    return Promise<User>({ resolve, reject ->
+        console.log("Logging into realm object server $server_url as user $username")
+        User.login(server_url, username, password, { error, user ->
+            if (error != null) {
+                console.log("Error logging in")
+                reject(Exception("Error logging in"))
+            }
+
+            if (user != null) {
+                console.log("Successfully logged in")
+                resolve(user)
+            }
+
+            reject(Exception("No error, but no user returned after login."))
         })
     })
 }
 
+/**
+ * Private function which opens a realm for a given user.
+ * @return Promise that resolves to a realm.
+ */
 fun openRealm(user: User): Promise<Realm> {
     val syncConfig = object : SyncConfiguration {
         override var user = user
